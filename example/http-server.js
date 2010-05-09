9ca7133c08a36a1c4c1b5138a66374916a385225
cloudhead <self@cloudhead.net>
2010-05-09 18:39:41 -0400

    updated example

--- example/http-server.js
+++ example/http-server.js
@@ -2,20 +2,26 @@ var path = require('path');
 
 require.paths.unshift(path.join(__dirname, '..', 'lib'));
 
-var Router = require('journey').Router;
+var journey = require('journey');
 
-var router = new(Router)(function (map) {
-    map.get('/').bind(function (res) { res.send("Welcome") });
+//
+// Create a Router object with an associated routing table
+//
+var router = new(journey.Router)(function (map) {
+    map.root.bind(function (res) { res.send("Welcome") }); // GET '/'
+    map.get('/version').bind(function (res) {
+        res.send(200, {}, { version: journey.version.join('.') });
+    });
 });
-
-var sys = require('sys');
 
 require('http').createServer(function (request, response) {
     var body = "";
 
     request.addListener('data', function (chunk) { body += chunk });
-
     request.addListener('end', function () {
+        //
+        // Dispatch the request to the router
+        //
         router.route(request, body, function (result) {
             response.writeHead(result.status, result.headers);
             response.end(result.body);
